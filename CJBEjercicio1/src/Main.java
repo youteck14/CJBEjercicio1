efcce078338f60e1911b5e23007d954c61d6e847
yohan <yohanrr86@gmail.com>
2022-07-01 15:03:03 -0500

    Adding exercise Java Basico

---
+++ CJBEjercicio1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeroEntero = 12423655;
+        long numeroLong = 1241242155;
+        double numeroDouble = 214.2421;
+        String nombre = "Yohan";
+
+        System.out.println(numeroEntero);
+        System.out.println(numeroLong);
+        System.out.println(numeroDouble);
+        System.out.println(nombre);
+    }
+}
